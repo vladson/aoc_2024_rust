6ca88aea4123e1cf498495b0c8e2dac6f921556a
Vladislav Bogomolov <v.bogomolov@synthace.com>
2024-12-10 11:53:51 +0000

    day 08 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -37,26 +37,46 @@ struct Antennae {
 }
 
 impl Antennae {
-    fn first_antinode(
+    fn first_antinode<F>(
         a1: &Antennae,
         a2: &Antennae,
-        m: usize,
-        n: usize,
-    ) -> Vec<Option<(usize, usize)>> {
+        bc: F
+    ) -> Vec<Option<(usize, usize)>> 
+    where F: Fn((i32, i32)) -> bool {
         let mut res = Vec::with_capacity(2);
         let i = a1.i as i32 + (a1.i as i32 - a2.i as i32);
         let j = a1.j as i32 + (a1.j as i32 - a2.j as i32);
-        if i < 0 || j < 0 || i >= m as i32 || j >= n as i32 {
-            res.push(None);
-        } else {
+        if bc((i, j)) {
             res.push(Some((i as usize, j as usize)));
+        } else {
+            res.push(None);
         }
         let i = a2.i as i32 + (a2.i as i32 - a1.i as i32);
         let j = a2.j as i32 + (a2.j as i32 - a1.j as i32);
-        if i < 0 || j < 0 || i >= m as i32 || j >= n as i32 {
-            res.push(None);
-        } else {
+        if bc((i, j)) {
             res.push(Some((i as usize, j as usize)));
+        } else {
+            res.push(None);
+        }
+        res
+    }
+
+    fn resonant_antinodes<F>(a1: &Antennae, a2: &Antennae, bc: F) -> Vec<Option<(usize, usize)>>
+    where
+        F: Fn((i32, i32)) -> bool,
+    {
+        let mut res = Vec::new();
+        let d1 = (a2.i as i32 - a1.i as i32, a2.j as i32 - a1.j as i32);
+        let mut node = (a1.i as i32 + d1.0, a1.j as i32 + d1.1);
+        while bc(node) {
+            res.push(Some((node.0 as usize, node.1 as usize)));
+            node = (node.0 + d1.0, node.1 + d1.1);
+        }
+        let d2 = (a1.i as i32 - a2.i as i32, a1.j as i32 - a2.j as i32);
+        let mut node = (a2.i as i32 + d2.0, a2.j as i32 + d2.1);
+        while bc(node) {
+            res.push(Some((node.0 as usize, node.1 as usize)));
+            node = (node.0 + d2.0, node.1 + d2.1);
         }
         res
     }
@@ -111,7 +131,6 @@ fn parse_input<R: BufRead>(reader: R) -> ([Vec<Antennae>; 256], Vec<Vec<Plot>>) 
     (frequencies, layofland)
 }
 fn main() -> Result<()> {
-
     start_day(DAY);
 
     //region Part 1
@@ -119,13 +138,16 @@ fn main() -> Result<()> {
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         let (frequencies, mut layofland) = parse_input(reader);
         let (m, n) = (layofland.len(), layofland[0].len());
+        let bound_checker = |node: (i32, i32)| {
+            !(node.0 < 0 || node.1 < 0 || node.0 >= m as i32 || node.1 >= n as i32)
+        };
         let ans = frequencies
             .iter()
             .filter(|&f| !f.is_empty())
             .fold(0, |mut acc, freq| {
                 for i in 0..freq.len() {
                     for j in i + 1..freq.len() {
-                        for point in Antennae::first_antinode(&freq[i], &freq[j], m, n) {
+                        for point in Antennae::first_antinode(&freq[i], &freq[j], &bound_checker) {
                             match point {
                                 None => {}
                                 Some(point) => match layofland[point.0][point.1] {
@@ -133,7 +155,7 @@ fn main() -> Result<()> {
                                         layofland[point.0][point.1] = Plot::Antinode;
                                         acc += 1
                                     }
-                                    _ => continue
+                                    _ => continue,
                                 },
                             }
                         }
@@ -153,17 +175,47 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let (frequencies, mut layofland) = parse_input(reader);
+        let (m, n) = (layofland.len(), layofland[0].len());
+        let bound_checker = |node: (i32, i32)| {
+            !(node.0 < 0 || node.1 < 0 || node.0 >= m as i32 || node.1 >= n as i32)
+        };
+        let ans = frequencies
+            .iter()
+            .filter(|&f| !f.is_empty())
+            .fold(0, |mut acc, freq| {
+                for i in 0..freq.len() {
+                    for j in i + 1..freq.len() {
+                        for point in
+                            Antennae::resonant_antinodes(&freq[i], &freq[j], &bound_checker)
+                        {
+                            match point {
+                                None => {}
+                                Some(point) => match layofland[point.0][point.1] {
+                                    Plot::Empty | Plot::Node(_) => {
+                                        layofland[point.0][point.1] = Plot::Antinode;
+                                        acc += 1
+                                    }
+                                    _ => continue,
+                                },
+                            }
+                        }
+                    }
+                }
+                acc
+            });
+        print_land(&layofland);
+        Ok(ans)
+    }
+
+    assert_eq!(34, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
